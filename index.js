152ff6b1f0f84208f8d4f0e1b0b84945ac4c37c2
SubhajitBar <pvt.subha@gmail.com>
2024-04-10 19:38:49 +0530

    index 6 added

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ console.log("Index file");
 console.log("Index 2 added");
 console.log("Index 3 added");
 console.log("Index 4 added and modified");
-console.log('Index 5 added and modified')
+console.log('Index 5 added and modified');
+console.log('Index 6 added');
